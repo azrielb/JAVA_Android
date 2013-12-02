af502c8f02e96637033e23ee644ada8c4824ee29
Azriel Berger <azrielb@gmail.com>
2013-12-02 22:11:23 +0200

    add try+catch in update fields

--- src/control/_Activity.java
+++ src/control/_Activity.java
@@ -5,12 +5,19 @@ import android.widget.TextView;
 
 public class _Activity extends Activity {
 	protected void setText(int TextViewID, String text) {
-		TextView theObject = (TextView) findViewById(TextViewID);
-		theObject.setText(text);
+		try {
+			TextView theObject = (TextView) findViewById(TextViewID);
+			theObject.setText(text);
+		} catch (Exception e) {
+		}
 	}
 
 	protected void appendText(int TextViewID, String text) {
-		TextView theObject = (TextView) findViewById(TextViewID);
-		theObject.setText(theObject.getText() + text);
+		try {
+			TextView theObject = (TextView) findViewById(TextViewID);
+			theObject.setText(theObject.getText() + text);
+		} catch (Exception e) {
+		}
+
 	}
 }
